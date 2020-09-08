9e2897b7f88dd4683c94ff7d8b73e8302521733d
saejung <sss804@naver.com>
2020-09-08 18:02:01 +0900

    mvc

--- router.js
+++ router.js
@@ -5,3 +5,9 @@ export const userRouter = express.Router();
 userRouter.get("/", (req, res) => res.send("user Home"));
 userRouter.get("/edit", (req, res) => res.send("user Edit"));
 userRouter.get("/password", (req, res) => res.send("user Password"));
+
+/*
+    M data
+    V how does the data look
+    C function that looks for the data
+*/
